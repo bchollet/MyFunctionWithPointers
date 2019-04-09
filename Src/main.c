afeabe5a1468abc7f1834368e5fa60f021e939cc
Bastian Chollet <bastian.chollet@cpnv.ch>
2019-04-09 10:24:59 +0200

    Replace INT functions with pointers function

--- Src/main.c
+++ Src/main.c
@@ -6,34 +6,27 @@ void sliceMinutes (); //Déclaration de fonction
 //L'utilisateur entre un nombre de minutes qui va ensuite être découpé en heure et en minutes restantes.
 int main()
 {
-    int nbhours = 0, nbminutes = 0;
+    int minutes = 0, hours = 0;
 
     printf("Veuillez entrer un nombre de minutes: ");
-    scanf("%i", &nbminutes);
+    scanf("%i", &minutes);
 
-    nbhours = sliceMinutesToHours(nbhours, nbminutes);
-    nbminutes = sliceMinutesToMinutes(nbminutes);
+    sliceMinutes(&hours, &minutes); //On envoit dans la fonction la valeur des adresses des variables
 
-    printf("%i heures et %i minutes", nbhours, nbminutes);
+    printf("%i heures et %i minutes", hours, minutes); //On imprime la valeur de la variable
 
     return EXIT_SUCCESS;
 }
 
 
-//Cette fonction va découper le nombre de minutes entré par l'utilisateur en heure.
-//Retourne le nombre d'heure
-int sliceMinutesToHours (int hours, int minutes)
+//Cette fonction va découper le nombre de minutes entré par l'utilisateur en heure et en minutes restantes.
+void sliceMinutes (int *pointerHour, int *pointerMinutes) //On déclare des pointeurs. On met donc un * devant le nom de la variable
 {
-    hours = minutes / 60; // Exemple: 90/60 = 1 vu que nous utilisons des entiers.
 
-    return hours;
-}
+    //Ces paramètres reçoivent une adressse mémoire.
+    //Or on désire travailler avec les valeur contenue à cette adresse.
+    //On ajoute donc un * pour obtenir la valeur stockée à l'adresse mémoire donnée.
 
-//Cette fonction va calculer le nombre de minutes restantes.
-//Retourne le nombre de minutes restantes
-int sliceMinutesToMinutes (int minutes)
-{
-    minutes = minutes % 60; // Exemple: 90%60 = 30.
-
-    return minutes;
+    *pointerHour = *pointerMinutes / 60; // Exemple: 90/60 = 1 vu que nous utilisons des entiers.
+    *pointerMinutes = *pointerMinutes % 60; // Exemple: 90%60 = 30.
 }
